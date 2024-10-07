02ed99cee1d72478a634af1b21433197b50993cb
seregabaranov <seregabaranov@yandex.ru>
2024-10-07 17:07:58 +0700

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -22,9 +22,9 @@ void createMatrix(int*** mas, unsigned n, unsigned m) {
 // mas -  указатель на матрицу
 // n - количество строк 
 // m - количество столбцов
-void inputMatrix(int *** mas, unsigned n, unsigned m) {
+void inputMatrix(int*** mas, unsigned n, unsigned m) {
 	if (mas == nullptr) createMatrix(mas, n, m);
-	cout << "enter matrix, line count = " << n << ", column count = "<< m << endl;
+	cout << "enter matrix, line count = " << n << ", column count = " << m << endl;
 	for (unsigned i = 0; i < n; i++) {
 		cout << "enter line number " << i << endl;
 		for (unsigned j = 0; j < m; j++)
@@ -50,10 +50,10 @@ void outputMatrix(int** mas, unsigned n, unsigned m) {
 // mas -  указатель на матрицу
 // n - количество строк 
 void deleteMatrix(int*** mas, unsigned n) {
-	if (mas!=nullptr) 
+	if (mas != nullptr)
 		for (unsigned i = 0; i < n; i++)
-		delete[] (*mas)[i];
-	
+			delete[](*mas)[i];
+
 	mas = nullptr;
 }
 
@@ -119,39 +119,39 @@ void getMatrixWithoutRowAndCol(int** mas, unsigned n, unsigned row, unsigned col
 // Нахождение определителя
 // mas - матрица
 // n - количество строк и столбцов матрицы
-int getDeterminant(int **mas, unsigned n) {
-    if (n > 0){
-        int det = 0;
-        int scalar = 1; 
-        
-        if(n == 1) {
-            return mas[0][0];
-        }
+int getDeterminant(int** mas, unsigned n) {
+	if (n > 0) {
+		int det = 0;
+		int scalar = 1;
 
-        if(n == 2) {
-            return mas[0][0]*mas[1][1] - mas[0][1]*mas[1][0];
-        }
-        
-        int **newmas = new int*[n-1];
-        for(int i = 0; i < n-1; i++) {
-            newmas[i] = new int[n-1];
-        }
+		if (n == 1) {
+			return mas[0][0];
+		}
 
-        for(int j = 0; j < n; j++) {
-            getMatrixWithoutRowAndCol(mas, n, 0, j, newmas);
+		if (n == 2) {
+			return mas[0][0] * mas[1][1] - mas[0][1] * mas[1][0];
+		}
 
-            det = det + (scalar * mas[0][j] * getDeterminant(newmas, n-1));
+		int** newmas = new int* [n - 1];
+		for (int i = 0; i < n - 1; i++) {
+			newmas[i] = new int[n - 1];
+		}
 
-            scalar = -scalar;
-        }
+		for (int j = 0; j < n; j++) {
+			getMatrixWithoutRowAndCol(mas, n, 0, j, newmas);
 
-        for(int i = 0; i < n-1; i++) {
-            delete [] newmas[i];
-        }
-        delete [] newmas;
-        
-        return det;
-    }
+			det = det + (scalar * mas[0][j] * getDeterminant(newmas, n - 1));
+
+			scalar = -scalar;
+		}
+
+		for (int i = 0; i < n - 1; i++) {
+			delete[] newmas[i];
+		}
+		delete[] newmas;
+
+		return det;
+	}
 }
 
 
@@ -186,21 +186,22 @@ void outputMatrixToFile(int** mas, unsigned n, unsigned m, const string& filenam
 //rowMatrix2 - строка матрицы(что прибавляем)
 //element - элемент матрицы
 void summationRowsMatrix(int*** matrix, size_t rows, size_t cols, size_t rowMatrix1, size_t rowMatrix2) {
-    if(matrix == nullptr) {
-        return;
-    }
-    
-    //Проверка входных данных на корректность
-    if(rowMatrix1 < 0 || rowMatrix2 < 0 || rows < 0 || cols < 0
-       || rowMatrix2 > rows || rowMatrix1 > rows) {
-        std::cout << "Выход за пределы массива" << std::endl;
-    } else {
-        for (int j = 0; j < cols; ++j) {
-            (*matrix)[rowMatrix1][j] += (*matrix)[rowMatrix2][j];
-        }
-    }
-    
-    return;
+	if (matrix == nullptr) {
+		return;
+	}
+
+	//Проверка входных данных на корректность
+	if (rowMatrix1 < 0 || rowMatrix2 < 0 || rows < 0 || cols < 0
+		|| rowMatrix2 > rows || rowMatrix1 > rows) {
+		std::cout << "Выход за пределы массива" << std::endl;
+	}
+	else {
+		for (int j = 0; j < cols; ++j) {
+			(*matrix)[rowMatrix1][j] += (*matrix)[rowMatrix2][j];
+		}
+	}
+
+	return;
 }
 
 //Сложение двух заданных столбцов матрицы
@@ -210,21 +211,22 @@ void summationRowsMatrix(int*** matrix, size_t rows, size_t cols, size_t rowMatr
 //colMatrix1 - столбец матрицы(куда прибавляем)
 //colMatrix2 - столбец матрицы(что прибавляем)
 void summationColsMatrix(int*** matrix, size_t rows, size_t cols, size_t colMatrix1, size_t colMatrix2) {
-    if(matrix == nullptr) {
-        return;
-    }
-    
-    //Проверка входных данных на корректность
-    if(colMatrix1 < 0 || colMatrix2 < 0 || rows < 0 || cols < 0
-       || colMatrix2 > rows || colMatrix1 > rows) {
-        std::cout << "Выход за пределы массива" << std::endl;
-    } else {
-        for (int i = 0; i < rows; ++i) {
-            (*matrix)[i][colMatrix1] += (*matrix)[i][colMatrix2];
-        }
-    }
-    
-    return;
+	if (matrix == nullptr) {
+		return;
+	}
+
+	//Проверка входных данных на корректность
+	if (colMatrix1 < 0 || colMatrix2 < 0 || rows < 0 || cols < 0
+		|| colMatrix2 > rows || colMatrix1 > rows) {
+		std::cout << "Выход за пределы массива" << std::endl;
+	}
+	else {
+		for (int i = 0; i < rows; ++i) {
+			(*matrix)[i][colMatrix1] += (*matrix)[i][colMatrix2];
+		}
+	}
+
+	return;
 }
 
 //Сложение двух матриц
@@ -235,23 +237,24 @@ void summationColsMatrix(int*** matrix, size_t rows, size_t cols, size_t colMatr
 //rows2 - количество строк матрицы2
 //cols2 - количество столбцов матрицы2
 void summationMatrix(int*** matrix1, size_t rows1, size_t cols1, int*** matrix2, size_t rows2, size_t cols2) {
-    
-    if(matrix1 == nullptr || matrix2 == nullptr) {
-        return;
-    }
-    
-    //Проверка возможности сложения матриц
-    if(rows1 == rows2 && cols1 == cols2) {
-        for(int i = 0; i < rows1; ++i) {
-            for(int j = 0; j < cols2; ++j) {
-                (*matrix1)[i][j] += (*matrix2)[i][j];
-            }
-        }
-    } else {
-        std::cout << "Матрицы невозможно сложить" << std::endl;
-    }
-    
-    return;
+
+	if (matrix1 == nullptr || matrix2 == nullptr) {
+		return;
+	}
+
+	//Проверка возможности сложения матриц
+	if (rows1 == rows2 && cols1 == cols2) {
+		for (int i = 0; i < rows1; ++i) {
+			for (int j = 0; j < cols2; ++j) {
+				(*matrix1)[i][j] += (*matrix2)[i][j];
+			}
+		}
+	}
+	else {
+		std::cout << "Матрицы невозможно сложить" << std::endl;
+	}
+
+	return;
 }
 
 // Умножение строки матрицы на целое число  
@@ -263,15 +266,15 @@ void summationMatrix(int*** matrix1, size_t rows1, size_t cols1, int*** matrix2,
 // result - изменённая матрица
 
 void multiplyingRowByNumber(int** mas, unsigned rows, unsigned columns, unsigned numRow, int num, int*** result) {
-    if (mas == nullptr) return;
-    if (numRow < 0) return;
+	if (mas == nullptr) return;
+	if (numRow < 0) return;
 
-    for (unsigned i = 0; i < rows; i++)
-        for (unsigned j = 0; j < columns; j++)
-            if (i == numRow)
-                (*result)[i][j] = mas[i][j] * num;
-            else
-                (*result)[i][j] = mas[i][j];
+	for (unsigned i = 0; i < rows; i++)
+		for (unsigned j = 0; j < columns; j++)
+			if (i == numRow)
+				(*result)[i][j] = mas[i][j] * num;
+			else
+				(*result)[i][j] = mas[i][j];
 }
 // Умножение столбца матрицы на целое число  
 // mas - матрица
@@ -282,15 +285,15 @@ void multiplyingRowByNumber(int** mas, unsigned rows, unsigned columns, unsigned
 // result - изменённая матрица
 
 void multiplyingColumnByNumber(int** mas, unsigned rows, unsigned columns, unsigned numColumn, int num, int*** result) {
-    if (mas == nullptr) return;
-    if (numColumn < 0) return;
+	if (mas == nullptr) return;
+	if (numColumn < 0) return;
 
-    for (unsigned i = 0; i < rows; i++)
-        for (unsigned j = 0; j < columns; j++)
-            if (j == numColumn)
-                (*result)[i][j] = mas[i][j] * num;
-            else
-                (*result)[i][j] = mas[i][j];
+	for (unsigned i = 0; i < rows; i++)
+		for (unsigned j = 0; j < columns; j++)
+			if (j == numColumn)
+				(*result)[i][j] = mas[i][j] * num;
+			else
+				(*result)[i][j] = mas[i][j];
 }
 
 // Умножение матрицы на целое число  
@@ -301,11 +304,11 @@ void multiplyingColumnByNumber(int** mas, unsigned rows, unsigned columns, unsig
 // result - изменённая матрица
 
 void multiplyingMatrixByNumber(int** mas, unsigned rows, unsigned columns, int num, int*** result) {
-    if (mas == nullptr) return;
+	if (mas == nullptr) return;
 
-    for (unsigned i = 0; i < rows; i++)
-        for (unsigned j = 0; j < columns; j++)
-            (*result)[i][j] = (mas[i][j]) * num;
+	for (unsigned i = 0; i < rows; i++)
+		for (unsigned j = 0; j < columns; j++)
+			(*result)[i][j] = (mas[i][j]) * num;
 }
 
 // количество попарно несовпадающих элементов в двух строках
@@ -315,16 +318,16 @@ void multiplyingMatrixByNumber(int** mas, unsigned rows, unsigned columns, int n
 // secondSize - кол-во элементов во второй строке
 int countPairwiseDifferentElementsTwoRows(int* firstRow, unsigned firstSize, int* secondRow, unsigned secondSize) {
 
-    unsigned count = 0; 
-    
-    if (firstRow == nullptr || secondRow == nullptr) return count;
+	unsigned count = 0;
 
-    unsigned cols = min(firstSize, secondSize);
+	if (firstRow == nullptr || secondRow == nullptr) return count;
 
-    for (int i = 0; i < cols; i++)
-        if (firstRow[i] != secondRow[i]) count++;
+	unsigned cols = min(firstSize, secondSize);
 
-    return count;
+	for (int i = 0; i < cols; i++)
+		if (firstRow[i] != secondRow[i]) count++;
+
+	return count;
 }
 
 // количество попарно несовпадающих элементов в двух матрицах
@@ -335,68 +338,123 @@ int countPairwiseDifferentElementsTwoRows(int* firstRow, unsigned firstSize, int
 // secRows - количество строк во второй матрице
 // secCols - количество столбцов во второй матрице
 int countPairwiseDifferentElementsTwoMatrices(int** firstMas, unsigned firstRows, unsigned firstCols, int** secMas, unsigned secRows, unsigned secCols) {
-    
-    unsigned count = 0;
 
-    if (firstMas == nullptr || secMas == nullptr) return 0;
+	unsigned count = 0;
 
-    unsigned rows = min(firstRows, secRows);
+	if (firstMas == nullptr || secMas == nullptr) return 0;
 
-    for (int i = 0; i < rows; i++)
-        count += countPairwiseDifferentElementsTwoRows(firstMas[i], firstCols, secMas[i], secCols);
+	unsigned rows = min(firstRows, secRows);
 
-    return count;
+	for (int i = 0; i < rows; i++)
+		count += countPairwiseDifferentElementsTwoRows(firstMas[i], firstCols, secMas[i], secCols);
+
+	return count;
 }
 
-// удаление строки матрицы со сдвигом 
-// mas - указатель на матрицу
 // n - количество строк 
 // m - количество столбцов
 // num - номер удаляемой строки
 int** deleteLine(int** mas, int n, int m, int num) {
-    if (n <= num) return mas; // проверка на наличие удаляемой строки в матрице
-    int** out = new int* [n - 1]; // создание новой матрицы
-    for (int i = 0, k = 0; i < n; i++, k++) {
-        if (i == num) k--; // в случае достижения номера удаляемой строки мы пропускаем итерацию
-        else {
-            out[k] = new int[m]; // создание строки
-            for (int j = 0; j < m; j++) { // заполнение элементами матрицы
-                out[k][j] = mas[i][j];
-            }
-        }
-    }
-    return out;
+	if (n <= num) return mas; // проверка на наличие удаляемой строки в матрице
+	int** out = new int* [n - 1]; // создание новой матрицы
+	for (int i = 0, k = 0; i < n; i++, k++) {
+		if (i == num) k--; // в случае достижения номера удаляемой строки мы пропускаем итерацию
+		else {
+			out[k] = new int[m]; // создание строки
+			for (int j = 0; j < m; j++) { // заполнение элементами матрицы
+				out[k][j] = mas[i][j];
+			}
+		}
+	}
+	return out;
 }
 
+
+int menu() {
+	int result;
+	cout << " 1 - createMatrix" << endl;
+	cout << " 2 - inputMatrix" << endl;
+	cout << " 3 - outputMatrix" << endl;
+	cout << " 4 - outputMatrixToFile" << endl;
+	cout << " 5 - getDeterminant" << endl;
+	cout << " 6 - multiplyingRowByNumber" << endl;
+	cout << " 7 - multiplyingColumnByNumber" << endl;
+	cout << " 8 - multiplyingMatrixByNumber" << endl;
+
+	cout << " 0 - exit" << endl;
+
+	cin >> result;
+	return result;
+}
 // основная программа
 int main()
 {
-	int** mas = nullptr, n = 2, m = 2;
-    int** result = nullptr;
+	setlocale(0, "");
+	int** mas = nullptr, ** mas2 = nullptr, n = 0, m = 0, row, column, number;
+	string filename;
 
-    createMatrix(&result, n, m);
+	while (1) {
 
-
-	createMatrix(&mas, n, m);
-	inputMatrix(&mas, n, m);
-	outputMatrix(mas, n, m);
-
-	outputMatrixToFile(mas, n, m, "mas.txt");
-
-	cout << "getDeterminant=" << getDeterminant(mas, n) << endl;
-
-    cout << "multiplying0RowByNumber2=" << endl;
-    multiplyingRowByNumber(mas, n, m, 0, 2, &result);
-    outputMatrix(result, n, m);
-
-    cout << "multiplying1ColumnByNumber5=" << endl;
-    multiplyingColumnByNumber(mas, n, m, 1, 5, &result);
-    outputMatrix(result, n, m);
-
-    cout << " multiplyingMatrixByNumber10=" << endl;
-    multiplyingMatrixByNumber(mas, n, m, 10, &result);
-    outputMatrix(result, n, m);
+		switch (menu())
+		{
+		case 1:
+			cout << "enter n, m:";
+			cin >> n >> m;
+			createMatrix(&mas, n, m);
+			system("cls");
+			cout << "createMatrix - done\n\n";
+			break;
+		case 2:
+			inputMatrix(&mas, n, m);
+			system("cls");
+			cout << "createMatrix - done\n\n";
+			break;
+		case 3:
+			system("cls");
+			outputMatrix(mas, n, m);
+			cout << "outputMatrix - done\n\n";
+			break;
+		case 4:
+			cout << "enter filename:";
+			cin >> filename;
+			outputMatrixToFile(mas, n, m, filename);
+			system("cls");
+			cout << "outputMatrixToFile - done\n\n";
+			break;
+		case 5:
+			system("cls");
+			cout << "getDeterminant=" << getDeterminant(mas, n) << endl;
+			break;
+		case 6:
+			cout << "enter row, number:";
+			cin >> row >> number;
+			createMatrix(&mas2, n, m);
+			multiplyingRowByNumber(mas, n, m, row, number, &mas2);
+			system("cls");
+			outputMatrix(mas2, n, m);
+			break;
+		case 7:
+			cout << "enter column, number:";
+			cin >> column >> number;
+			createMatrix(&mas2, n, m);
+			multiplyingColumnByNumber(mas, n, m, column, number, &mas2);
+			system("cls");
+			outputMatrix(mas2, n, m);
+			break;
+		case 8:
+			cout << "enter number:";
+			cin >> number;
+			createMatrix(&mas2, n, m);
+			multiplyingMatrixByNumber(mas, n, m, number, &mas2);
+			system("cls");
+			outputMatrix(mas2, n, m);
+			break;
+		case 0: return 0;
+			break;
+		}
+	}
 
 	deleteMatrix(&mas, n);
+	deleteMatrix(&mas2, n);
 }
 
